9340e2e52be1e25aceba7b7835437cfe1628a5c4
Gavin <gavinvm@protonmail.com>
2024-12-16 15:25:30 +0200

    feat: Render posters and description

--- public/js/movie-search.js
+++ public/js/movie-search.js
@@ -1,3 +1,55 @@
+const clear = function clearDropdown() {
+  const dropdownContainer = document.getElementById('movie-dropdown-container');
+  while (dropdownContainer.firstChild) {
+    dropdownContainer.removeChild(dropdownContainer.firstChild);
+  }
+};
+
+const populate = function populateMovieDropDown(movies) {
+  const dropdownContainer = document.getElementById('movie-dropdown-container');
+  clear();
+
+  movies.slice(0, 5).forEach((movie) => {
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    // Create poster
+    const poster = document.createElement('img');
+    poster.setAttribute(
+      'src',
+      `https://image.tmdb.org/t/p/w500/${movie.poster_path}` ||
+        'default-poster.jpg'
+    );
+    poster.setAttribute('alt', movie.original_title || 'Movie Poster');
+
+    // Create title and description container
+    const titleAndDescription = document.createElement('div');
+    titleAndDescription.classList.add('title-description');
+
+    // Add title
+    const titleElement = document.createElement('h3');
+    titleElement.textContent = movie.original_title || 'Unknown Title';
+
+    // Add description
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent =
+      movie.overview || 'No description available.';
+
+    // Append title and description to container
+    titleAndDescription.appendChild(titleElement);
+    titleAndDescription.appendChild(descriptionElement);
+
+    // Append all elements to the movie container
+    movieContainer.appendChild(poster);
+    movieContainer.appendChild(titleAndDescription);
+
+    // Append movie container to dropdown
+    dropdownContainer.appendChild(movieContainer);
+  });
+
+  return;
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
   const movieInput = document.getElementById('movie-search');
   let abortController = null;
@@ -27,7 +79,8 @@ document.addEventListener('DOMContentLoaded', async () => {
           { ...options, signal: abortController.signal }
         );
         const data = await response.json();
-        console.log(data);
+        console.log(data.results);
+        populate(data.results);
       } catch (error) {
         error.name === 'AbortError'
           ? console.log('Request was aborted')
@@ -38,8 +91,10 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Call fetchmoviedata
     movieInput.addEventListener('input', (e) => {
       const query = e.target.value;
-      if (query) {
+      if (query !== '') {
         fetchMovieData(query);
+      } else {
+        clear();
       }
     });
   } catch (error) {
